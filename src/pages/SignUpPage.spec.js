896fcbef640241764fb4734b75dcd232f375a356
tquyen2109 <t.quyen1997@yahoo.com>
2022-07-27 23:54:49 -0400

    Initial commit

---
+++ src/pages/SignUpPage.spec.js
@@ -0,0 +1,86 @@
+import SignUpPage from "./SignUpPage";
+import {render, screen} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import axios from "axios";
+describe("Sign Up Page", () => {
+    describe("Layout", () => {
+        it("has header", () => {
+            render(<SignUpPage />);
+            const header = screen.queryByRole("heading",{ name: "Sign Up"});
+            expect(header).toBeInTheDocument();
+        });
+        it("has username input", () => {
+            render(<SignUpPage />);
+            const input = screen.getByLabelText("Username");
+            expect(input).toBeInTheDocument();
+        });
+        it("has email input", () => {
+            render(<SignUpPage />);
+            const input = screen.getByLabelText("E-mail");
+            expect(input).toBeInTheDocument();
+        });
+        it("has password input", () => {
+            render(<SignUpPage />);
+            const input = screen.getByLabelText("Password");
+            expect(input).toBeInTheDocument();
+        });
+        it("has password type for password input", () => {
+            render(<SignUpPage />);
+            const input = screen.getByLabelText("Password");
+            expect(input.type).toBe("password");
+        });
+        it("has password repeat input", () => {
+            render(<SignUpPage />);
+            const input = screen.getByLabelText("Password Repeat");
+            expect(input).toBeInTheDocument();
+        });
+        it("has password type for password repeat input", () => {
+            render(<SignUpPage />);
+            const input = screen.getByLabelText("Password Repeat");
+            expect(input.type).toBe("password");
+        });
+        it("has sign up button", () => {
+            render(<SignUpPage />);
+            const button = screen.queryByRole("button",{ name: "Sign Up"});
+            expect(button).toBeInTheDocument();
+        });
+        it("disable the button initially", () => {
+            render(<SignUpPage />);
+            const button = screen.queryByRole("button",{ name: "Sign Up"});
+            expect(button).toBeDisabled();
+        });
+    });
+    describe("Interactions", () => {
+        it('should enables the button when password and password repeat fields have same value', function () {
+            render(<SignUpPage />);
+            const passwordInput = screen.getByLabelText("Password");
+            const passwordRepeatInput = screen.getByLabelText("Password Repeat");
+            userEvent.type(passwordInput, "Password");
+            userEvent.type(passwordRepeatInput, "Password");
+            const button = screen.queryByRole("button",{ name: "Sign Up"});
+            expect(button).toBeEnabled();
+        });
+        it('should send username and password to backend after clicking the button', function () {
+            render(<SignUpPage />);
+            const usernameInput = screen.getByLabelText("Username");
+            const emailInput = screen.getByLabelText("E-mail");
+            const passwordInput = screen.getByLabelText("Password");
+            const passwordRepeatInput = screen.getByLabelText("Password Repeat");
+            userEvent.type(usernameInput, 'user1');
+            userEvent.type(emailInput, 'user1@mail.com');
+            userEvent.type(passwordInput, "Password");
+            userEvent.type(passwordRepeatInput, "Password");
+            const button = screen.queryByRole("button",{ name: "Sign Up"});
+            const mockFn = jest.fn();
+            axios.post = mockFn;
+            userEvent.click(button);
+            const firstCallOfMockFunction = mockFn.mock.calls[0];
+            const body = firstCallOfMockFunction[1];
+            expect(body).toEqual({
+                username: 'user1',
+                email: 'user1@mail.com',
+                password: 'Password'
+            })
+        });
+    });
+});
